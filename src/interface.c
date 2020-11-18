554bc0d347fdc1e36d5594a10cbd67bfe794f8e6
GuillaumeAndroid <guillaume.revel2000@gmail.com>
2020-11-18 10:09:38 +0100

    Subwin remplacé par newwin

--- src/interface.c
+++ src/interface.c
@@ -1,27 +1,41 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {	
 
     WINDOW *haut, *bas;
+    char *title = "Simulation épidémie";
     
     initscr();
-    haut= subwin(stdscr, LINES / 10, COLS, 0, 0);        // Créé une fenêtre de 'LINES / 2' lignes et de COLS colonnes en 0, 0
-    bas= subwin(stdscr, LINES * 9/10, COLS, LINES / 10, 0); // Créé la même fenêtre que ci-dessus sauf que les coordonnées changent
+    haut= newwin(LINES / 10, COLS, 0, 0);        // Créé une fenêtre de 'LINES / 2' lignes et de COLS colonnes en 0, 0
+    bas= newwin(LINES * 9/10, COLS, LINES / 10, 0); // Créé la même fenêtre que ci-dessus sauf que les coordonnées changent
     
 
     start_color();
     init_pair(1, COLOR_YELLOW, COLOR_GREEN);
+    
+
+    refresh();
+
+
     attron(COLOR_PAIR(1));
+
+    mvwprintw(haut, 0, (COLS / 2) - (strlen(title) / 2), title);
+
     attroff(COLOR_PAIR(1));
 
-    mvwprintw(haut, 1, 1, "Ceci est la fenetre du haut");
     mvwprintw(bas, 1, 1, "Ceci est la fenetre du bas");
+
+    
     
     wrefresh(haut);
     wrefresh(bas);
     
     getch();
+    delwin(haut);
+    delwin(bas);
     endwin();
  
     free(haut);
