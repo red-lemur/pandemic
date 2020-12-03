8a46e1e380c1dd2f60ea10d4003d57dda3805b87
jpoullain <jeremy.poullain@ecole.ensicaen.fr>
2020-12-03 21:58:19 +0100

    City generated, plus qu'à diviser la fonction en fonctions subsidiaires

--- src/epidemic_sim.c
+++ src/epidemic_sim.c
@@ -51,22 +51,25 @@ int create_shared_memory()
 
 void generate_city(city_t *city)
 {
-    /* lire un fichier pour trouver les coordonnées */
-    /* => les constantes HOUSES_NB, etc. deviendront inutiles */
-    /*city->map[0][6] = init_tile_firestation(0, 6);
-    city->map[3][3] = init_tile_hospital(3, 3);
-    city->map[3][0] = init_tile_firestation(6, 0);*/
     load_map(city);
+
+    // Générer citoyens ?
 }
 
-void load_map(city_t *city)
+void load_map(city_t *city) // A DIVISER
 {
     FILE *fp;
+    
     char buffer[100];
+    char building_type;
     
     int col;
-    int row = 0;
+    int row;
 
+    int indexes_taken[CITY_WIDTH][CITY_HEIGHT];
+    int buildings_nb;
+    int building;
+    
     double contamination;
     
     if ((fp = fopen(MAP_URL, "r")) == NULL) {
@@ -74,20 +77,38 @@ void load_map(city_t *city)
         exit(-1);
     }
 
+    for (row = 0; row < CITY_HEIGHT; row++) {
+        for (col = 0; col < CITY_HEIGHT; col++) {
+            indexes_taken[col][row] = 0;
+        }   
+    }
+    
+    row = 0;
     while (fscanf(fp, "%[^\n]\n", buffer) != EOF) {
-        if (buffer[0] == '\n' || (buffer[0] == '/' && buffer[1] == '/')) {
+        if (buffer[0] == '\n' || (buffer[0] == '/' && buffer[1] == '/')
+            || buffer[0] == ' ' || buffer[0] == '\t') {
             continue;
         }
-
+        
         if (row < CITY_HEIGHT) {
             for (col = 0; col < CITY_WIDTH; col++) {
                 switch (buffer[col]) {
-                case 'F' : city->map[col][row] = init_tile_firestation(col, row); break;
-                case 'H' : city->map[col][row] = init_tile_hospital(col, row); break;
-                case 'O' : city->map[col][row] = init_tile_house(col, row); break;
+                case 'F' :
+                    city->map[col][row] = init_tile_firestation(col, row);
+                    indexes_taken[col][row] = 1;
+                    break;
+                case 'H' :
+                    city->map[col][row] = init_tile_hospital(col, row);
+                    indexes_taken[col][row] = 1;
+                    break;
+                case 'O' :
+                    city->map[col][row] = init_tile_house(col, row);
+                    indexes_taken[col][row] = 1;
+                    break;
                 case 'W' :
                     contamination = generate_random_wasteland_contamination_level();
-                    city->map[col][row] = init_tile_wasteland(col, row, 0.0);
+                    city->map[col][row] = init_tile_wasteland(col, row, contamination);
+                    indexes_taken[col][row] = 1;
                     break;
                 case 'X' : break;
                 default :
@@ -96,30 +117,86 @@ void load_map(city_t *city)
                 }
             }
         }
-        else {
-                    
-            /* Gérer le random
-            switch (buffer[col]) {
-            case 'F' : city->map[col][row] = init_tile_firestation(col, row); break;
-            case 'H' : city->map[col][row] = init_tile_hospital(col, row); break;
-            case 'O' : city->map[col][row] = init_tile_house(col, row); break;
-            case 'W' : city->map[col][row] = init_tile_wasteland(col, row, 0.0); break;
-            default :
-                printf("Error while reading the map\n");
+        else {        
+            if (!sscanf(buffer, "%c %d", &building_type, &buildings_nb)) {
+                printf("Error while reading the map parameters\n");
                 exit(-1);
-                }*/
+            }
+
+            for (building = 0; building < buildings_nb; building++) {
+                if (all_tile_indexes_are_taken(indexes_taken)) {
+                    printf("Too many buildings on the map\n");
+                    exit(-1);
+                }
+                
+                do {
+                    col = generate_random_index(CITY_WIDTH);
+                    row = generate_random_index(CITY_HEIGHT);    
+                } while (indexes_taken[col][row]);
+                
+                switch (building_type) {                
+                case 'F' :
+                    city->map[col][row] = init_tile_firestation(col, row);
+                    indexes_taken[col][row] = 1;
+                    break;
+                case 'H' :
+                    city->map[col][row] = init_tile_hospital(col, row);
+                    indexes_taken[col][row] = 1;
+                    break;
+                case 'O' :
+                    city->map[col][row] = init_tile_house(col, row);
+                    indexes_taken[col][row] = 1;
+                    break;
+                default :
+                    printf("Error while reading the map\n");
+                    exit(-1);
+                }
+            }
         }
 
         row++;
     }
-    
+
     fclose(fp);
+    
+    if (!all_tile_indexes_are_taken(indexes_taken)) {
+        for (row = 0; row < CITY_HEIGHT; row++) {
+            for (col = 0; col < CITY_HEIGHT; col++) {
+                if (!indexes_taken[col][row]) {
+                    contamination = generate_random_wasteland_contamination_level();
+                    city->map[col][row] = init_tile_wasteland(col, row, contamination);
+                }
+            }   
+        }
+    }
 }
 
-double generate_random_wasteland_contamination_level() {
+double generate_random_wasteland_contamination_level()
+{
     return rand()/(double)RAND_MAX * (MAX_WASTELAND_CONTAMINATION_AT_BEGINNING
                                       - MIN_WASTELAND_CONTAMINATION_AT_BEGINNING)
         + MIN_WASTELAND_CONTAMINATION_AT_BEGINNING;
+}
+
+int generate_random_index(int ind_max)
+{
+    return (int)(rand()/(double)RAND_MAX * ind_max);
+}
+
+int all_tile_indexes_are_taken(int indexes_taken[CITY_WIDTH][CITY_HEIGHT])
+{
+    int row;
+    int col;
+    
+    for (row = 0; row < CITY_HEIGHT; row++) {
+        for (col = 0; col < CITY_HEIGHT; col++) {
+            if (!indexes_taken[col][row]) {
+                return 0;
+            }
+        }   
+    }
+    
+    return 1;
 }
 
 /* mqd_t create_mqueue(); */
@@ -138,6 +215,15 @@ int main(void)
 
     city = mmap(NULL, sizeof(city_t), PROT_READ | PROT_WRITE, MAP_SHARED, mem, 0);
     generate_city(city);
+
+    /* TEST DEBUG MAP */
+    /*int row, col;
+    for (row = 0; row < CITY_HEIGHT; row++) {
+        for (col = 0; col < CITY_HEIGHT; col++) {
+            printf("%d", city->map[col][row].type);
+        }
+        printf("\n");
+    }*/
     
     /* Début de la simulation */
     /* C'est à vous d'écrire là ... */
